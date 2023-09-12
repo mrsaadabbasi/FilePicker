b0070829fdbdece6fcc46a7e170a131c02f27628
rehmasaa <rehmasaa@CRLHL-REHMASAA1>
2023-09-12 18:48:36 +0500

    Updating App tsx file

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
+import * as React from 'react';
 
-import { StyleSheet, View, Text, Button } from 'react-native'
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 import DocumentPicker, {
   DirectoryPickerResponse,
@@ -8,80 +8,31 @@ import DocumentPicker, {
   isCancel,
   isInProgress,
   types,
-} from 'react-native-document-picker'
-import { useEffect,useState } from 'react'
+} from 'react-native-document-picker';
+import {useEffect} from 'react';
 
 export default function App() {
   //var fileReader = new (require('react-native-filereader'))();
-  const [singleFile, setSingleFile] = useState(null);
   const [result, setResult] = React.useState<
     Array<DocumentPickerResponse> | DirectoryPickerResponse | undefined | null
-  >()
+  >();
 
   useEffect(() => {
-    debugger;
-    console.log("Result is "+JSON.stringify(result, null, 2))
-    setSingleFile(result);
-    uploadImage();
-    // const fileToUpload = singleFile;
-    // const data = new FormData();
-    // data.append('name', 'File Upload');
-    // data.append('file_attachment', fileToUpload[0]);
-    // console.log("Data is "+JSON.stringify(data, null, 2))
-    // let res = fetch(
-    //   'https://localhost:44342/weatherforecast',
-    //   {
-    //     method: 'post',
-    //     body: data,
-    //     headers: {
-    //       'Content-Type': 'multipart/form-data; ',
-    //     },
-    //   }
-    // );
-    // let responseJson = res.json();
-   // var res = fileReader.readAsDataURL(result.uri);
-   // console.log(res)
-  }, [result])
-
-  const uploadImage = async () => {
-    // Check if any file is selected or not
-    if (singleFile != null) {
-      // If file selected then create FormData
-      const fileToUpload = singleFile;
-      const data = new FormData();
-      data.append('name', 'Image Upload');
-      data.append('file_attachment', fileToUpload[0]);
-      // Please change file upload URL
-      let res = await fetch(
-        'https://localhost:44342/weatherforecast',
-        {
-          method: 'get',
-          // body: data,
-          // headers: {
-          //   'Content-Type': 'multipart/form-data; ',
-          // },
-        }
-      );
-      let responseJson = await res.json();
-      if (responseJson.status == 1) {
-        alert('Upload Successful');
-      }
-    } else {
-      // If no file selected the show alert
-      alert('Please Select File first');
-    }
-  };
+    console.log('Result is ' + JSON.stringify(result, null, 2));
+  }, [result]);
 
   const handleError = (err: unknown) => {
     if (isCancel(err)) {
-      console.warn('cancelled')
+      console.warn('cancelled');
       // User cancelled the picker, exit any dialogs or menus and move on
     } else if (isInProgress(err)) {
-      console.warn('multiple pickers were opened, only the last will be considered')
+      console.warn(
+        'multiple pickers were opened, only the last will be considered',
+      );
     } else {
-      throw err
+      throw err;
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
@@ -92,17 +43,19 @@ export default function App() {
             const pickerResult = await DocumentPicker.pickSingle({
               presentationStyle: 'fullScreen',
               copyTo: 'cachesDirectory',
-            })
-            setResult([pickerResult])
+            });
+            setResult([pickerResult]);
           } catch (e) {
-            handleError(e)
+            handleError(e);
           }
         }}
       />
       <Button
         title="open picker for multi file selection"
         onPress={() => {
-          DocumentPicker.pick({ allowMultiSelection: true }).then(setResult).catch(handleError)
+          DocumentPicker.pick({allowMultiSelection: true})
+            .then(setResult)
+            .catch(handleError);
         }}
       />
       <Button
@@ -113,7 +66,7 @@ export default function App() {
             type: [types.doc, types.docx],
           })
             .then(setResult)
-            .catch(handleError)
+            .catch(handleError);
         }}
       />
       <Button
@@ -123,7 +76,7 @@ export default function App() {
             type: types.pdf,
           })
             .then(setResult)
-            .catch(handleError)
+            .catch(handleError);
         }}
       />
       <Button
@@ -131,21 +84,21 @@ export default function App() {
         onPress={() => {
           DocumentPicker.releaseSecureAccess([])
             .then(() => {
-              console.warn('releaseSecureAccess: success')
+              console.warn('releaseSecureAccess: success');
             })
-            .catch(handleError)
+            .catch(handleError);
         }}
       />
       <Button
         title="open directory picker"
         onPress={() => {
-          DocumentPicker.pickDirectory().then(setResult).catch(handleError)
+          DocumentPicker.pickDirectory().then(setResult).catch(handleError);
         }}
       />
 
       <Text selectable>Result: {JSON.stringify(result, null, 2)}</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -160,4 +113,4 @@ const styles = StyleSheet.create({
     height: 60,
     marginVertical: 20,
   },
-})
+});
